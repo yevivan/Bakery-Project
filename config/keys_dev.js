4d00a2c40221a36870525a6702516bf679c26ed6
Yevhen <yevivan@gmail.com>
2022-10-19 11:46:52 +0300

    backend to repository

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Saribeg:ksg888489Final7@ksgastound-hlrd0.mongodb.net/FE_Final_Project_BE?retryWrites=true&w=majority",
+  secretOrKey: "random very very secret string"
+};
