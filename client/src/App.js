e77e700f81d7bbedb068cf16795c7279a724b8ed
Yevhen <yevivan@gmail.com>
2022-10-19 16:27:08 +0300

    setting Eslint as default formatter

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let`s start our Bakery-project `
         </a>
       </header>
     </div>
