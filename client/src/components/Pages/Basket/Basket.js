c5a2b847eec261a4dda80db4e2017be14156d984
Roman Brus <gabrielrom89@gmail.com>
2022-10-25 14:15:30 +0300

    add BascetModule

--- client/src/components/Pages/Basket/Basket.js
+++ client/src/components/Pages/Basket/Basket.js
@@ -1,13 +1,18 @@
+import { useSelector, shallowEqual } from 'react-redux';
+import { Container } from '@mui/material';
 import BasketList from '../../BasketList/BasketList';
 import OrderForm from '../../OrderForm/OrderForm';
+import styles from './Basket.module.scss';
 
 function Basket() {
+  const data = useSelector((state) => state.data.data, shallowEqual);
+  console.log(data);
   return (
-    <div>
-      <p>КОРЗИНА</p>
+    <Container className={styles.basket}>
+      <h1 className={styles.title}>Моя корзина</h1>
       <BasketList />
       <OrderForm />
-    </div>
+    </Container>
   );
 }
 export default Basket;
