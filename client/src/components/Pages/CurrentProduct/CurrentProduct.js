e706dcc1423e785ba09efb616aa2ba18b22ae10e
Anna Fiedosieieva <ann991999fevral@gmail.com>
2022-10-31 19:08:39 +0200

    fix adaptive

--- client/src/components/Pages/CurrentProduct/CurrentProduct.js
+++ client/src/components/Pages/CurrentProduct/CurrentProduct.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
 import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
+import { Grid, Typography, Box } from '@mui/material';
 import styles from './CurrentProduct.module.scss';
 import ProductsCounter from '../../PoductsCounter/ProductsCounter';
 import { addBasketArr } from '../../../store/slices/basketArr';
@@ -26,17 +27,89 @@ function CurrentProduct() {
   };
 
   return (
-    <div className={styles.card__container}>
-      <div className={styles.card__image}>
-        <img className={styles.image} src={picture} alt="" />
-      </div>
-      <div className={styles.card__info}>
-        <p className={styles.title}>{name}</p>
-        <p className={styles.price}>
+    <Grid
+      className={styles.card__container}
+      container
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' },
+      }}
+      flexWrap="nowrap"
+      padding="0"
+      m="0"
+      display="flex"
+      direction={{
+        md: 'row',
+        lg: 'column',
+      }}
+      margin="75px auto 75px auto"
+      gap="60px"
+      width="100%"
+    >
+      <Grid
+        className={styles.card__image}
+        item
+        container
+        xs
+        margin="0 auto 0 auto"
+      >
+        <Box
+          width="100%"
+          height="500px"
+          m="0"
+          sx={{
+            backgroundImage: `url(${picture})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+      </Grid>
+      <Grid
+        className={styles.card__info}
+        item
+        container
+        xs
+        margin="0 auto 0 auto"
+        display="flex"
+        flexDirection="column"
+      >
+        <Typography
+          variant="h2"
+          align="left"
+          color="#391113"
+          fs="48px"
+          fontFamily="Asap"
+          fontWeight="600"
+          lineHeight="1.2"
+          textTransform="capitalize"
+          m="0"
+        >
+          {name}
+        </Typography>
+        <Typography
+          variant="h4"
+          align="left"
+          color="#494949"
+          mt="20px"
+          fs="26px"
+          fontFamily="Asap"
+          fontWeight="lighter"
+          lineHeight="1.2"
+        >
           {price}
-          <span>&#8372;</span>
-        </p>
-        <div className={styles.parameters}>
+          &#8372;
+        </Typography>
+        <Grid
+          className={styles.parameters}
+          container
+          mt="15px"
+          sx={{
+            maxWidth: '235px',
+            display: 'flexbox',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
           <CardGiftcardIcon
             onClick={() => {
               setGiftWrap(true);
@@ -44,9 +117,13 @@ function CurrentProduct() {
             }}
           />
           {giftWrap ? (
-            <p style={activeParameters}>Gift wrap</p>
+            <Grid item xs style={activeParameters}>
+              Gift wrap
+            </Grid>
           ) : (
-            <p>Gift wrap</p>
+            <Grid item xs>
+              Gift wrap
+            </Grid>
           )}
           <LocalMallOutlinedIcon
             onClick={() => {
@@ -55,21 +132,57 @@ function CurrentProduct() {
             }}
           />
           {regularWrap ? (
-            <p style={activeParameters}>Regular wrap</p>
+            <Grid item xs style={activeParameters}>
+              Regular wrap
+            </Grid>
           ) : (
-            <p>Regular wrap</p>
+            <Grid item xs>
+              Regular wrap
+            </Grid>
           )}
-        </div>
-        <div className={styles.products__counter}>
-          <ProductsCounter />
-        </div>
-        <div className={styles.about}>
+        </Grid>
+        <ProductsCounter />
+        <Typography
+          variant="body1"
+          align="left"
+          color="#494949"
+          fs="14px"
+          fontFamily="Asap"
+          fontWeight="lighter"
+          lineHeight="1.2"
+          mt="20px"
+          width="100%"
+          sx={{
+            textIndent: '15px',
+          }}
+        >
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit ex
           corporis eos molestias veniam animi, numquam illum autem, beatae
           tenetur, necessitatibus qui. Quae voluptates suscipit laudantium
           dolorem id quam nemo?
-        </div>
+        </Typography>
         <Button
+          sx={{
+            mt: '20px',
+            width: '100%',
+            height: '54px',
+            backgroundColor: '#391113',
+            color: '#fa9bc9',
+            fontFamily: 'Barlow Condensed',
+            lineHeight: '1.40',
+            fontWeight: '200',
+            fontSize: '23px',
+            textTransform: 'capitalize',
+            borderRadius: '0',
+            '&:hover': {
+              backgroundColor: '#391113',
+              opacity: '0.6',
+            },
+            '&:disabled': {
+              color: '#391113',
+              backgroundColor: '#fff5d6',
+            },
+          }}
           disabled={!counter || !(giftWrap || regularWrap)}
           variant="contained"
           className={styles.btn}
@@ -81,8 +194,8 @@ function CurrentProduct() {
         >
           Add To Basket
         </Button>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
 export default CurrentProduct;
