01f18bfe155f1aa24f00ab78dc6ed28d4459218c
Виталик Демкив <xdemkivx@gmail.com>
2022-11-03 21:03:15 +0200

    add adaptiv header

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -4,16 +4,18 @@ import styles from './Menu.module.scss';
 
 function Menu() {
   return (
-    <div className={styles.menu}>
-      <MenuIcon />
-      <Navigation
-        catalog={<p>All item</p>}
-        category1={<p>category1</p>}
-        category2={<p>category2</p>}
-        category3={<p>category3</p>}
-        category4={<p>category4</p>}
-      />
-    </div>
+    <>
+      <MenuIcon className={styles.burgerMenu} />
+      <div className={styles.menu}>
+        <Navigation
+          catalog={<p>All item</p>}
+          category1={<p>category1</p>}
+          category2={<p>category2</p>}
+          category3={<p>category3</p>}
+          category4={<p>category4</p>}
+        />
+      </div>
+    </>
   );
 }
 export default Menu;
