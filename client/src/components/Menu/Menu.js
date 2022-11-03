23f0ecfa3252deb33c9792809316095b5a6c51c2
Виталик Демкив <xdemkivx@gmail.com>
2022-11-03 18:16:29 +0200

    add burger menu

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,15 +1,17 @@
+import MenuIcon from '@mui/icons-material/Menu';
 import Navigation from '../Navigation/NavigationMenu';
 import styles from './Menu.module.scss';
 
 function Menu() {
   return (
     <div className={styles.menu}>
+      <MenuIcon />
       <Navigation
-        catalog={<p>весь каталог/веде на масив всіх item</p>}
-        category1={<p>категорія</p>}
-        category2={<p>категорія</p>}
-        category3={<p>категорія</p>}
-        category4={<p>категорія</p>}
+        catalog={<p>All item</p>}
+        category1={<p>category1</p>}
+        category2={<p>category2</p>}
+        category3={<p>category3</p>}
+        category4={<p>category4</p>}
       />
     </div>
   );
