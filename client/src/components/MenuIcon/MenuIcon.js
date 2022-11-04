183bfe5a842801cde7fb42bd598908cde182c769
Виталик Демкив <xdemkivx@gmail.com>
2022-11-04 17:46:47 +0200

    add hower menu

--- client/src/components/MenuIcon/MenuIcon.js
+++ client/src/components/MenuIcon/MenuIcon.js
@@ -1,9 +1,10 @@
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import Navigation from '../Navigation/NavigationIconMenu';
+import styles from './MenuIcon.module.scss';
 
 function MenuIcon() {
   return (
-    <div>
+    <div className={styles.img}>
       <Navigation basket={<ShoppingBasketIcon />} />
     </div>
   );
