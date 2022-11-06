4c5a66b4b532dcc90019acda307a35d0025556d4
Pavlo Nechai <nechaipavel7@gmail.com>
2022-11-06 19:15:21 +0200

    fix footer-form

--- client/src/components/FormsUI/Textfield/Textfield.js
+++ client/src/components/FormsUI/Textfield/Textfield.js
@@ -1,55 +1,50 @@
-/* eslint-disable */
 /* eslint-disable react/jsx-props-no-spreading */
 
 // ... your code here
 import React from 'react';
-// import { TextField } from '@material-ui/core';
+import TextField from '@mui/material/TextField';
 import { useField } from 'formik';
-// import { withStyles } from '@material-ui/core/styles';
 
-// const CssTextField = withStyles({
-//   root: {
-//     '& label.Mui-focused': {
-//       color: '#391113',
-//     },
-//     '& .MuiInput-underline:after': {
-//       borderBottomColor: '#f09ec4',
-//     },
-//     '& .MuiOutlinedInput-root': {
-//       '& fieldset': {
-//         borderColor: '#d8cbc0',
-//         border: '3px solid #d8cbc0',
-//       },
-//       '&:hover fieldset': {
-//         borderColor: '#f09ec4',
-//       },
-//       '&.Mui-focused fieldset': {
-//         borderColor: '#f09ec4',
-//       },
-//     },
-//   },
-// })(TextField);
+function TextfieldWrapper({ name, ...otherProps }) {
+  const [field, mata] = useField(name);
 
-// function Textfield({ name, ...otherProps }) {
-//   const [field, mata] = useField(name);
+  const configTextfield = {
+    ...field,
+    ...otherProps,
+    fullWidth: true,
+    variant: 'outlined',
+  };
 
-//   const configTextfield = {
-//     ...field,
-//     ...otherProps,
-//     fullWidth: true,
-//     variant: 'outlined',
-//   };
+  if (mata && mata.touched && mata.error) {
+    configTextfield.error = true;
+    configTextfield.helperText = mata.error;
+  }
 
-//   if (mata && mata.touched && mata.error) {
-//     configTextfield.error = true;
-//     configTextfield.helperText = mata.error;
-//   }
-
-//   return <CssTextField multiline {...configTextfield} variant="outlined" />;
-// }
-
-// export default Textfield;
-
-export default function TextField () {
-  return null
+  return (
+    <TextField
+      {...configTextfield}
+      sx={{
+        '& label.Mui-focused': {
+          color: '#391113',
+        },
+        '& .MuiInput-underline:after': {
+          borderBottomColor: '#f09ec4',
+        },
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: '#d8cbc0',
+            border: '3px solid #d8cbc0',
+          },
+          '&:hover fieldset': {
+            borderColor: '#f09ec4',
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: '#f09ec4',
+          },
+        },
+      }}
+    />
+  );
 }
+
+export default TextfieldWrapper;
