5b91e658511e1ca81d3793cc5bd1cfa74c9b4bbd
Олена Чижевська <chizhevskaya2305@gmail.com>
2022-11-09 13:27:08 +0200

    BasketList fixed and CurrentProduct

--- client/src/components/Pages/CurrentProduct/CurrentProduct.js
+++ client/src/components/Pages/CurrentProduct/CurrentProduct.js
@@ -9,23 +9,23 @@ import styles from './CurrentProduct.module.scss';
 import ProductsCounter from '../../PoductsCounter/ProductsCounter';
 import { addBasketArr } from '../../../store/slices/basketArr';
 
-// function CurrentProduct() {
-//   // const { id } = props;
-//   const dispatch = useDispatch();
+function CurrentProduct() {
+  // const { id } = props;
+  const dispatch = useDispatch();
 
-//   const name = 'tasty cake';
-//   const price = '250';
-//   const picture = 'https://www.bakerstreetbakery.com.ua/image/cache/data/torty/shvarzvald/schwarzwald%201-500x500.jpg';
-//   const id = 'cakeeee';
+  const name = 'tasty cake';
+  const price = '250';
+  const picture = 'https://www.bakerstreetbakery.com.ua/image/cache/data/torty/shvarzvald/schwarzwald%201-500x500.jpg';
+  const id = 'cakeeee';
 
-//   const [giftWrap, setGiftWrap] = useState(false);
-//   const [regularWrap, setRegularWrap] = useState(false);
-//   const counter = useSelector((store) => store.counterProducts.counterProducts);
-//   const basket = useSelector((store) => store.basketArr.basketArr);
-//   const activeParameters = {
-//     color: '#391113',
-//     borderBottom: '2px solid #fa9bc9',
-//   };
+  const [giftWrap, setGiftWrap] = useState(false);
+  const [regularWrap, setRegularWrap] = useState(false);
+  const counter = useSelector((store) => store.counterProducts.counterProducts);
+  const basket = useSelector((store) => store.basketArr.basketArr);
+  const activeParameters = {
+    color: '#391113',
+    borderBottom: '2px solid #fa9bc9',
+  };
   return (
     <Grid
       className={styles.card__container}
@@ -196,3 +196,4 @@ import { addBasketArr } from '../../../store/slices/basketArr';
     </Grid>
   );
 }
+export default CurrentProduct;
