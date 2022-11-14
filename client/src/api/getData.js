7e03901dd3dbcbee1a7e9cc2ec4c51c62c52c049
yevivan <91620904+yevivan@users.noreply.github.com>
2022-11-14 16:09:04 +0200

    front connected to back

--- client/src/api/getData.js
+++ client/src/api/getData.js
@@ -12,3 +12,17 @@ export const getData = async () => {
     isPopular,
   }));
 };
+
+// export const getData = async () => {
+//   const arrayData = await fetch('./data.json').then((res) => res.json());
+//   return arrayData.map(({
+//     category, name, price, picture, id, IsFavorite,
+//   }) => ({
+//     category,
+//     name,
+//     price,
+//     picture,
+//     id,
+//     IsFavorite,
+//   }));
+// };
