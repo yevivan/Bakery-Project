987decbcfa1b1ec62353a3b3274aad00af9c9d69
yevivan <91620904+yevivan@users.noreply.github.com>
2022-12-15 04:42:51 +0200

    server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const comments = require("./routes/comments");
 const shippingMethods = require("./routes/shippingMethods");
 const paymentMethods = require("./routes/paymentMethods");
 const partners = require("./routes/partners");
-const mainRoute = require("./routes/index.js");
+const mainRoute = require("./routes/index");
 
 const app = express();
 
@@ -72,14 +72,14 @@ app.use("/partners", partners);
 app.use("/", mainRoute);
 
 // Server static assets if in production
-
+if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-
+}
 
 const port = process.env.PORT || 5005;
 console.log(process.env.PORT);
