478c14ca993d57541952d83e5e23d8b79e64e743
yevivan <91620904+yevivan@users.noreply.github.com>
2022-12-15 04:18:29 +0200

    server

--- server.js
+++ server.js
@@ -72,16 +72,16 @@ app.use("/partners", partners);
 app.use("/", mainRoute);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === "production") {
+
   // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
 
-const port =  5005;
-console.log(process.env.NODE_ENV);
 
-app.listen(port, () => console.log(` running on port ${port}`));
+const port = process.env.PORT || 5005;
+console.log(process.env.PORT);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
