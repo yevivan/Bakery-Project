067968fa20cebdf72ec2ed14fc8ca9ed528ecbbb
yevivan <91620904+yevivan@users.noreply.github.com>
2022-12-23 19:56:56 +0200

    update after review dy DAN It

--- client/src/api/getCart.js
+++ client/src/api/getCart.js
@@ -6,13 +6,3 @@ export const getCart = async () => {
     .then((res) => res.json());
   return cart;
 };
-
-// For postman to put product in cart
-
-// {"products": [
-//     {
-//       "product": "637499250149e4cc3ba25c5d",
-//       "cartQuantity": 21
-//     }
-//   ]
-//   }
