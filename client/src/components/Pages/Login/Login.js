90b1de1951440520f5943d6950eef47c074d061f
yevivan <91620904+yevivan@users.noreply.github.com>
2023-01-25 17:41:47 -0500

    update

--- client/src/components/Pages/Login/Login.js
+++ client/src/components/Pages/Login/Login.js
@@ -79,7 +79,7 @@ function Login() {
             // dispatch(getCartItems());
           });
           resetForm();
-          navigate('/');
+          // navigate('/');
         }}
       >
         {({ isValid }) => (
